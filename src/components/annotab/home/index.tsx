47035cf0e8d4d6c9f8f2dcd8825f978ce9b1b064
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2024-01-31 16:17:05 +0700

    feat(base template): base template

--- src/components/annotab/home/index.tsx
+++ src/components/annotab/home/index.tsx
@@ -1,5 +1,55 @@
 'use client';
 
+import { createColumnHelper } from '@tanstack/react-table';
+import { useState } from 'react';
+
+import CoreTable from '../table';
+
+const columnHelper = createColumnHelper<any>();
+
+const defaultColumns = [
+  columnHelper.accessor((row) => row.id, {
+    id: 'id',
+    cell: (info) => info.getValue(),
+    header: () => <span>ID</span>,
+    footer: (props) => props.column.id,
+  }),
+  columnHelper.accessor((row) => row.role, {
+    id: 'role',
+    cell: (info) => info.getValue(),
+    header: () => <span>Role</span>,
+    footer: (props) => props.column.id,
+  }),
+  columnHelper.accessor((row) => row.user.email, {
+    id: 'email',
+    cell: (info) => info.getValue(),
+    header: () => <span>Email</span>,
+    footer: (props) => props.column.id,
+  }),
+  columnHelper.accessor((row) => row.status, {
+    id: 'status',
+    cell: (info) => info.getValue(),
+    header: () => <span>Status</span>,
+    footer: (props) => props.column.id,
+  }),
+];
+
 export default function Home() {
-  return <>Dashboard</>;
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+
+  return (
+    <div className="m-4">
+      <CoreTable
+        data={[]}
+        columns={defaultColumns}
+        loading={false}
+        page={page}
+        pageSize={pageSize}
+        total={0}
+        setPage={setPage}
+        setPageSize={setPageSize}
+      />
+    </div>
+  );
 }

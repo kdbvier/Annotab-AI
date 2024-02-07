a073d232b0c4bcd4ac2575c562f53d633d9874b2
Chetan Bardoliya <workspace.chetan@gmail.com>
2024-02-07 19:31:42 +0530

    fix(layout structure changed): layout structure changed
    layout structure changed

--- src/components/class-management/class-table/index.tsx
+++ src/components/class-management/class-table/index.tsx
@@ -4,43 +4,41 @@ const ClassTable = () => {
   return (
     <div>
       <h1 className="pb-5 text-base font-semibold text-[#535572]">Class 01</h1>
-
       <div className="max-w-[342px] rounded-lg border bg-[#FCFCFF]">
         <div className="m-4 h-[20px] max-w-[312px] rounded-lg bg-[#686299]" />
         <table className="m-4">
-          <tr>
-            <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
-              Type of class:
-            </td>
-            <td className=" pl-14 text-sm font-normal text-[#31374A]">
-              Bounding Box
-            </td>
-          </tr>
-
-          <tr>
-            <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
-              Date created:
-            </td>
-            <td className="  pl-14 text-sm font-normal text-[#31374A]">
-              11/03/2023
-            </td>
-          </tr>
-
-          <tr>
-            <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
-              Number of class:
-            </td>
-            <td className="  pl-14 text-sm font-normal text-[#31374A]">12</td>
-          </tr>
-
-          <tr>
-            <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
-              Sub-annotation class:
-            </td>
-            <td className=" W-full  pl-14 text-sm font-normal text-[#31374A]">
-              XXX
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
+                Type of class:
+              </td>
+              <td className=" pl-14 text-sm font-normal text-[#31374A]">
+                Bounding Box
+              </td>
+            </tr>
+            <tr>
+              <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
+                Date created:
+              </td>
+              <td className="  pl-14 text-sm font-normal text-[#31374A]">
+                11/03/2023
+              </td>
+            </tr>
+            <tr>
+              <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
+                Number of class:
+              </td>
+              <td className="  pl-14 text-sm font-normal text-[#31374A]">12</td>
+            </tr>
+            <tr>
+              <td className=" max-w-[160px] text-sm font-semibold text-[#31374A]">
+                Sub-annotation class:
+              </td>
+              <td className=" W-full  pl-14 text-sm font-normal text-[#31374A]">
+                XXX
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>

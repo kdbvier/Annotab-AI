69ef0190ca917b211208034af99bafc9789b0bad
TriPham9001 <tri.pham1101@gmail.com.com>
2024-02-08 13:03:20 +0700

    feat(dataset detail): front end

---
+++ src/components/annotab/dataset/detail/index.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { Tab } from '@headlessui/react';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import React, { useMemo, useState } from 'react';
+
+import Data from './data';
+
+interface ListTabProps {
+  label: string;
+  layout: JSX.Element;
+}
+
+interface DatasetDetailProps {
+  datasetId: string;
+}
+
+const DatasetDetail = ({ datasetId }: DatasetDetailProps) => {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const listTab = useMemo(
+    () => [
+      {
+        label: 'Data',
+        layout: <Data datasetId={datasetId} userId={datasetId} />,
+      },
+      {
+        label: 'Workflow',
+        layout: <div>Workflow</div>,
+      },
+      {
+        label: 'Version',
+        layout: <div>Version</div>,
+      },
+    ],
+    []
+  );
+
+  return (
+    <div className="block h-full w-full">
+      <Tab.Group as="div" className="h-full w-full">
+        <Tab.List className="flex w-full flex-row">
+          {listTab.map((item: ListTabProps, index: number) => (
+            <Tab
+              key={item.label}
+              onClick={() => setCurrentTab(index)}
+              className={({ selected }) =>
+                clsx(
+                  ' w-1/3 py-3 text-base font-bold leading-5 text-dark-navy-blue focus:outline-none',
+                  selected
+                    ? 'relative bg-transparent'
+                    : 'bg-mostly-white dark:bg-purple-grey'
+                )
+              }
+            >
+              {item.label}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels as="div" className="h-full w-full">
+          {listTab.map((item: ListTabProps, index: number) => (
+            <Tab.Panel key={item.label} className="h-full w-full">
+              {currentTab === index && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1.3 }}
+                  className="h-[calc(100vh-104px)] w-full overflow-y-auto overflow-x-hidden px-7 py-4 text-black"
+                >
+                  {item.layout}
+                </motion.div>
+              )}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+};
+
+export default DatasetDetail;

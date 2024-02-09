84b15bb42317ce9d6e8a9d6a363ed96010457f4f
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2024-02-09 16:47:28 +0700

    feat(update): update

--- src/app/(default)/settings/account/page.tsx
+++ src/app/(default)/settings/account/page.tsx
@@ -1,6 +1,5 @@
 import { dehydrate, QueryClient } from '@tanstack/query-core';
 import { HydrationBoundary } from '@tanstack/react-query';
-import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 
 import SettingsAccount from '@/components/annotab/settings/account';
@@ -30,15 +29,7 @@ export default async function AccountSetting() {
   });
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="w-2/4 pt-6 2xl:w-3/4">
-        <Link
-          href="/"
-          className="mb-[120px] ml-auto block w-[260px] rounded-lg border border-gray-100 bg-gray-100 px-8 py-1 text-dark-navy-blue hover:bg-gray-100/80 hover:text-black/80"
-        >
-          Go to your personal setting
-        </Link>
-        <SettingsAccount />
-      </div>
+      <SettingsAccount />
     </HydrationBoundary>
   );
 }
--- src/components/annotab/settings/billing/index.tsx
+++ src/components/annotab/settings/billing/index.tsx
@@ -14,10 +14,10 @@ const SettingsBilling = () => {
   const { data } = useCurrentWorkspace(session?.user.access.token);
 
   return (
-    <div className="flex h-full w-full flex-col gap-y-5 overflow-y-auto px-7 py-11">
+    <div className="w-2/4 pt-6 2xl:w-3/4">
       <Link
         href="/"
-        className="mb-[120px] ml-auto block rounded-lg border border-gray-100 bg-gray-100 px-8 py-1 text-dark-navy-blue hover:bg-gray-100/80 hover:text-black/80"
+        className="mb-[120px] ml-auto block w-[260px] rounded-lg border border-gray-100 bg-gray-100 px-8 py-1 text-dark-navy-blue hover:bg-gray-100/80 hover:text-black/80"
       >
         Go to your personal setting
       </Link>

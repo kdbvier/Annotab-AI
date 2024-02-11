3bfd615e172dcd9eb50eee59f60aa051117d8a78
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2024-02-12 00:28:22 +0700

    feat(update): update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,5 +2,5 @@ import Middleware from 'next-auth/middleware';
 
 export default Middleware;
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next|sign-in|sign-up).*)', '/'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next|sign-in|sign-up|verify-token).*)', '/'],
 };

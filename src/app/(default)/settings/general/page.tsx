a76d616d3963563db06d1b22c2a368e0312eb3ba
Alex Nguyen <alex.nguyen@alfagomma.com.sg>
2024-02-12 01:22:59 +0700

    feat(update): update

--- src/app/(default)/settings/general/page.tsx
+++ src/app/(default)/settings/general/page.tsx
@@ -1,26 +1,5 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from '@tanstack/react-query';
-import { getServerSession } from 'next-auth';
-
 import GeneralSettings from '@/components/annotab/settings/general';
-import { fetchCurrentWorkspace } from '@/hooks/queries/useCurrentWorkspace';
-import { authOptions } from '@/libs/auth';
 
 export default async function GeneralSettingsPage() {
-  const session = await getServerSession(authOptions);
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['currentWorkspace', session?.user.access.token],
-    queryFn: () => fetchCurrentWorkspace(session?.user.access.token),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <GeneralSettings />
-    </HydrationBoundary>
-  );
+  return <GeneralSettings />;
 }
